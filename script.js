45eb28bf3a8a2f5d737d2ca34adf6ee3ade248e6
madoka <menomonies@gmail.com>
2015-05-18 23:50:47 -0700

    Submit simple-todo-list

---
+++ script.js
@@ -0,0 +1,24 @@
+
+//submit text in the text box
+var form = document.querySelector("form");
+form.addEventListener("submit", function(e) {
+	e.preventDefault();
+	toDoList();
+	
+});
+
+//get values from inputs
+
+var button = document.querySelector("button");
+button.addEventListener("click", function(){
+	toDoList();
+});
+
+function toDoList(){
+	var input = document.getElementById("input");
+	var li = document.createElement("li");
+	li.innerHTML = input.value;
+	var list = document.getElementById("toDoList");
+	list.appendChild(li);
+	input.value = "";
+}
